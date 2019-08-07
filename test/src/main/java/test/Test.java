d7a3755a99c98bd9ae7d0212e0020da4db8ef441
iwona <iovonkagawel@gmail,com>
2019-08-07 12:33:39 +0200

    cz1

---
+++ test/src/main/java/test/Test.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Test {
+
+    private int size;
+    private Kot kot;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Kot getKot() {
+        return kot;
+    }
+
+    public void setKot(Kot kot) {
+        this.kot = kot;
+    }
+}
